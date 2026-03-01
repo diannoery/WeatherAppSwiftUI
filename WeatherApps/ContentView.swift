4fc7c6e3619d6e60c97f3aff85bbbdcae96167c3
DianNoery <Diannoery29@gmail.com>
2026-03-01 12:50:38 +0700

    Initial Commit

---
+++ WeatherApps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApps
+//
+//  Created by Avows Technology on 01/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApps/WeatherAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppsApp.swift
+//  WeatherApps
+//
+//  Created by Avows Technology on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
